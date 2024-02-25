7edc1bbe22eaa92e9718f8610161d488d491c435
Marcelo Augusto <mrcelo014@gmail.com>
2024-02-25 09:14:08 -0300

    Refactor code for improved readability and fix syntax error

---
+++ front-end/to-do-list/src/components/NoTaskCreated/index.tsx
@@ -0,0 +1,13 @@
+import { ButtonCreateTask } from "../Tasks/ButtonCrateTask";
+
+export const NoTaskCreated = () => {
+    return (
+        <div className="flex justify-center flex-col items-center w-full h-full">
+            <h1 className="text-4xl">No task created :(</h1>
+            <div className="flex items-center text-3xl gap-4">
+                Create task
+                <ButtonCreateTask />
+            </div>
+        </div>
+    );
+};
